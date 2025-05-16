191da2ef720c2f9e2a658ae41744ee2b8a48999e
Liam Greenway <ApplePiePc@outlook.com>
2025-05-16 07:32:14 -0400

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,247 @@
+use std::fmt::{Display, Formatter};
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+
+const ADTP_VERSION: &str = "ADTP/0.0";
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        println!("{}", Request::create(Method::Read).with_content_string("Hello World".to_string(), ContentType::Text).build());
-        println!("{}", Response::create(ResponseCode::Ok).with_content_string("Hello World".to_string(), ContentType::Text).build());
-        panic!();
-    }    
+    fn requests_test() {
+        let request = Request::create(Method::Create, "buss://napture.web".to_string(), "test/0.0".to_string()).with_content_string(ContentType::Text, "Content".to_string()).with_language("us-english".to_string());
+        assert_eq!(request.clone().build().as_str(), Request::try_from(request.build()).unwrap().build().as_str());
+    }
+    
+    #[test]
+    fn responses_test() {
+        let response = Response::create(Status::Ok, "test/0.0".to_string()).with_language("us-english".to_string()).with_content_string(ContentType::Text, "Content".to_string());
+        assert_eq!(response.clone().build().as_str(), Response::try_from(response.build()).unwrap().build().as_str());
+    }
 }
 
 #[derive(Debug,Clone, Serialize, Deserialize)]
 pub enum Method {
     /// Used to read existing data
+    #[serde(rename = "read")]
     Read,
     /// Used to create new data
-    Insert,
+    #[serde(rename = "create")]
+    Create,
     /// Used to update existing data
-    Set,
+    #[serde(rename = "update")]
+    Update,
     /// Used to delete existing data
-    Remove,
+    #[serde(rename = "destroy")]
+    Destroy,
     /// Used to check if data exists
-    Check
+    #[serde(rename = "check")]
+    Check,
+    /// Used to authorize with a server
+    #[serde(rename = "auth")]
+    Auth
 }
 
 #[derive(Debug,Clone, Serialize, Deserialize)]
 pub enum ContentType {
-    Text, // Text in UTF-8
-    Binary, // Raw 1s and 0s
+    /// AAC Audio
+    #[serde(rename = "aac")]
+    AAC,
+    /// AVIF Image
+    #[serde(rename = "avif")]
+    AVIF,
+    /// Audio Video Interleaf
+    #[serde(rename = "avi")]
+    AVI,
+    /// Raw Binary Data
+    #[serde(rename = "binary")]
+    Binary,
+    /// Bitmap Image
+    #[serde(rename = "bmp")]
+    BMP,
+    /// Cascading Style Sheets in UTF-8
+    #[serde(rename = "css")]
+    CSS,
+    /// Comma Seperated Values in UTF-8
+    #[serde(rename = "csv")]
+    CSV,
+    /// Microsoft Word Document
+    #[serde(rename = "docx")]
+    DOCX,
+    /// EPUB Book
+    #[serde(rename = "epub")]
+    EPUB,
+    /// GIF Image
+    #[serde(rename = "gif")]
+    GIF,
+    /// HTML in UTF-8
+    #[serde(rename = "html")]
+    HTML,
+    /// Icon File
+    #[serde(rename = "ico")]
+    ICO,
+    /// JPEG Image File
+    #[serde(rename = "jpeg")]
+    JPEG,
+    /// JavaScript in UTF-8
+    #[serde(rename = "javascript")]
+    JavaScript,
+    /// JavaScript Object Notation in UTF-8
+    #[serde(rename = "json")]
+    JSON,
+    /// Markdown in UTF-8
+    #[serde(rename = "markdown")]
+    MarkDown,
+    /// MIDI
+    #[serde(rename = "midi")]
+    MIDI,
+    /// MP3 Audio
+    #[serde(rename = "mp3")]
+    MP3,
+    /// MP4 Video
+    #[serde(rename = "mp4")]
+    MP4,
+    /// MPEG Video
+    #[serde(rename = "mpeg")]
+    MPEG,
+    /// OpenDocument Presentation
+    #[serde(rename = "odp")]
+    ODP,
+    /// OpenDocument Spreadsheet
+    #[serde(rename = "ods")]
+    ODS,
+    /// OpenDocument Text
+    #[serde(rename = "odt")]
+    ODT,
+    /// Ogg Audio
+    #[serde(rename = "oga")]
+    OGA,
+    /// Ogg Video
+    #[serde(rename = "ogv")]
+    OGV,
+    /// Ogg
+    #[serde(rename = "ogx")]
+    OGX,
+    /// Opus audio in Ogg Container
+    #[serde(rename = "opus")]
+    Opus,
+    /// OpenType Font
+    #[serde(rename = "otf")]
+    OTF,
+    /// PNG Image
+    #[serde(rename = "png")]
+    PNG,
+    /// PDF Document
+    #[serde(rename = "pdf")]
+    PDF,
+    /// PHP Page
+    #[serde(rename = "php")]
+    PHP,
+    /// Microsoft PowerPoint Presentation
+    #[serde(rename = "pptx")]
+    PPTX,
+    /// RAR Archive
+    #[serde(rename = "rar")]
+    RAR,
+    /// SVG Graphics
+    #[serde(rename = "svg")]
+    SVG,
+    /// TAR Archive
+    #[serde(rename = "tar")]
+    TAR,
+    /// TIFF Image
+    #[serde(rename = "tiff")]
+    TIFF,
+    /// TrueType Font
+    #[serde(rename = "ttf")]
+    TTF,
+    /// Plain Text (UTF-8)
+    #[serde(rename = "text")]
+    Text,
+    /// Waveform Audio
+    #[serde(rename = "wav")]
+    WAV,
+    /// Webm Audio
+    #[serde(rename = "weba")]
+    WEBA,
+    /// Webm Video
+    #[serde(rename = "webm")]
+    WEBM,
+    /// Webm Image
+    #[serde(rename = "webp")]
+    WEBP,
+    /// Woff font (WOFF1 or WOFF2)
+    #[serde(rename = "woff")]
+    WOFF,
+    /// Microsoft Excel Spreadsheet
+    #[serde(rename = "xlsx")]
+    XLSX,
+    /// XML (UTF-8)
+    #[serde(rename = "xml")]
+    XML,
+    /// Zip Archive
+    #[serde(rename = "zip")]
+    ZIP,
+    /// 7z archive
+    #[serde(rename = "7z")]
+    SevenZ,
+    #[serde(rename = "none")]
     None
 }
 
 #[derive(Debug,Clone, Serialize, Deserialize)]
 pub struct Request {
-    version: i32,
+    #[serde(rename="version")]
+    version: String,
+    #[serde(rename="uri")]
+    pub uri: String,
+    #[serde(rename="sent")]
+    pub sent: DateTime<Utc>,
+    #[serde(rename="agent")]
+    pub agent: String,
+    #[serde(rename="method")]
     pub method: Method,
+    #[serde(rename="lang")]
+    pub language: String,
+    #[serde(rename="contentType")]
     pub content_type: ContentType,
+    #[serde(rename="content")]
     pub content: Vec<u8>,
 }
 impl Request {
-    pub fn create(method: Method) -> Request {
+    pub fn create(method: Method, uri: String, agent: String) -> Request {
         Request {
-            version: 1,
+            version: ADTP_VERSION.to_string(),
+            uri,
+            sent: Utc::now(),
+            agent,
+            language: "none".to_string(),
             method,
             content_type: ContentType::None,
             content: vec![],
         }
     }
     
-    pub fn with_content(mut self, content: Vec<u8>, content_type: ContentType) -> Request {
-        self.content = content;
-        self.content_type = content_type;
+    pub fn with_time(mut self, sent: DateTime<Utc>) -> Request {
+        self.sent = sent;
         self
     }
     
-    pub fn with_content_string(mut self, content: String, content_type: ContentType) -> Request {
-        self.content = content.into_bytes();
+    pub fn with_language(mut self, language: String) -> Request {
+        self.language = language;
+        self
+    }
+    
+    pub fn with_content(mut self, content_type: ContentType, content: Vec<u8>) -> Request {
         self.content_type = content_type;
+        self.content = content;
+        self
+    }
+
+    pub fn with_content_string(mut self, content_type: ContentType, content: String) -> Request {
+        self.content_type = content_type;
+        self.content = content.into_bytes();
         self
     }
     
@@ -66,57 +249,106 @@ impl Request {
         serde_json::to_string(&self).unwrap()
     }
 }
-impl From<String> for Request {
-    fn from(value: String) -> Self {
-        serde_json::from_str(&value).unwrap()
+impl TryFrom<String> for Request {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match serde_json::from_str(&value) {
+            Ok(r) => Ok(r),
+            Err(e) => Err(format!("Failed to parse JSON: {}", e)),
+        }
     }
 }
-impl From<Vec<u8>> for Request {
-    fn from(value: Vec<u8>) -> Self {
-        serde_json::from_str(&String::from_utf8(value).unwrap()).unwrap()
+impl TryFrom<Vec<u8>> for Request {
+    type Error = String;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        match String::from_utf8(value) { 
+            Ok(r) => match serde_json::from_str(r.as_str()) { 
+                Ok(r) => Ok(r),
+                Err(e) => Err(format!("Failed to parse JSON: {}", e)),
+            },
+            Err(e) => Err(format!("Failed to parse JSON: {}", e)),
+        }
+    }
+}
+impl Display for Request {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", serde_json::to_string(&self).unwrap())
     }
 }
 
 #[derive(Debug,Clone, Serialize, Deserialize)]
-pub enum ResponseCode {
+pub enum Status {
+    #[serde(rename="switch-protocols")]
     SwitchProtocols, // Server is switching protocol. Check content for protocol type
+    #[serde(rename="ok")]
     Ok, // Server has acknowledged your request.
+    #[serde(rename="pending")]
     Pending, // Request is pending. Wait for follow up.
+    #[serde(rename="redirect")]
     Redirect, // Content is no longer at requested location. Check content for new location.
+    #[serde(rename="denied")]
     Denied, // Server has denied request.
+    #[serde(rename="bad-request")]
     BadRequest, // Request is malformed.
+    #[serde(rename="unauthorized")]
     Unauthorized, // You are not authorized to view the content
+    #[serde(rename="not-found")]
     NotFound, // Server could not find the requested content.
+    #[serde(rename="too-many-requests")]
     TooManyRequests, // Server has received too many requests from this client
-    InternalServerError, // Server has encountered an error
+    #[serde(rename="internal-error")]
+    InternalError, // Server has encountered an error
 }
 
 #[derive(Debug,Clone, Serialize, Deserialize)]
 pub struct Response {
-    version: i32,
-    pub code: ResponseCode,
+    #[serde(rename="version")]
+    version: String,
+    #[serde(rename="status")]
+    pub status: Status,
+    #[serde(rename="sent")]
+    pub sent: DateTime<Utc>,
+    #[serde(rename="agent")]
+    pub agent: String,
+    #[serde(rename="language")]
+    pub language: String,
+    #[serde(rename="contentType")]
     pub content_type: ContentType,
+    #[serde(rename="content")]
     pub content: Vec<u8>,
 }
 impl Response {
-    pub fn create(code: ResponseCode) -> Response {
+    pub fn create(status: Status, agent: String) -> Response {
         Response {
-            version: 1,
-            code,
+            version: ADTP_VERSION.to_string(),
+            status,
+            sent: Utc::now(),
+            agent,
+            language: "none".to_string(),
             content_type: ContentType::None,
             content: vec![],
         }
     }
     
-    pub fn with_content(mut self, content: Vec<u8>, content_type: ContentType) -> Response {
-        self.content = content;
-        self.content_type = content_type;
+    pub fn with_time(mut self, sent: DateTime<Utc>) -> Response {
+        self.sent = sent;
         self
     }
     
-    pub fn with_content_string(mut self, content: String, content_type: ContentType) -> Response {
-        self.content = content.into_bytes();
+    pub fn with_language(mut self, language: String) -> Response {
+        self.language = language;
+        self
+    }
+    
+    pub fn with_content(mut self, content_type: ContentType, content: Vec<u8>) -> Response {
         self.content_type = content_type;
+        self.content = content;
+        self
+    }
+    
+    pub fn with_content_string(mut self, content_type: ContentType, content: String) -> Response {
+        self.content_type = content_type;
+        self.content = content.into_bytes();
         self
     }
     
@@ -124,13 +356,31 @@ impl Response {
         serde_json::to_string(&self).unwrap()
     }
 }
-impl From<String> for Response {
-    fn from(value: String) -> Self {
-        serde_json::from_str(&value).unwrap()
+
+impl TryFrom<String> for Response {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match serde_json::from_str(&value) {
+            Ok(r) => Ok(r),
+            Err(e) => Err(format!("Failed to parse JSON: {}", e)),
+        }
     }
 }
-impl From<Vec<u8>> for Response {
-    fn from(value: Vec<u8>) -> Self {
-        serde_json::from_str(&String::from_utf8(value).unwrap()).unwrap()
+impl TryFrom<Vec<u8>> for Response {
+    type Error = String;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        match String::from_utf8(value) {
+            Ok(r) => match serde_json::from_str(r.as_str()) {
+                Ok(r) => Ok(r),
+                Err(e) => Err(format!("Failed to parse JSON: {}", e)),
+            },
+            Err(e) => Err(format!("Failed to parse JSON: {}", e)),
+        }
+    }
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", serde_json::to_string(&self).unwrap())
     }
 }
